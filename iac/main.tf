52123595799d5b299a208775660937fa27a91e2b
Jonathas Sampaio <jonathas.sampaio.pos.puc@gmail.com>
2023-04-21 16:48:06 -0300

    Airflow via Terraform

--- iac/main.tf
+++ iac/main.tf
@@ -28,7 +28,46 @@ resource "google_storage_bucket" "buckets" {
   count    = length(var.lake_zones)
   name     = "${var.project_id}-${var.lake_zones[count.index]}"
   location = var.region
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
+
+resource "google_project_iam_member" "composer_worker" {
+  project = var.project_id
+  member  = "serviceAccount:ssa-tdah-composer@self-service-analytics-tdah.iam.gserviceaccount.com"
+  role    = "roles/composer.worker"
+}
+
+resource "google_project_iam_member" "editor" {
+  project = var.project_id
+  member  = "serviceAccount:877794803636@cloudservices.gserviceaccount.com"
+  role    = "roles/editor"
+}
+
+
+resource "google_composer_environment" "composer" {
+  project = var.project_id
+  name = "ssa-tdah-etl"
+  region = var.region
+
+  config {
+
+    software_config {
+      image_version = "composer-1.20.8-airflow-2.3.4"
+    }
+
+    node_config {
+      service_account = "ssa-tdah-composer@self-service-analytics-tdah.iam.gserviceaccount.com"
+    }
+
+  }
+}
+
+
+
+
 
 output "service_account_key" {
   value     = google_service_account_key.key.private_key
