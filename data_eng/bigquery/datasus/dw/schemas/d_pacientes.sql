bd7f05622af80926dbff42c42ad2e2dedd621cde
Jonathas Sampaio <jonathas.sampaio.pos.puc@gmail.com>
2023-04-29 18:31:12 -0300

    fix

--- data_eng/bigquery/datasus/dw/schemas/d_pacientes.sql
+++ data_eng/bigquery/datasus/dw/schemas/d_pacientes.sql
@@ -1,21 +1,31 @@
 CREATE TABLE `self-service-analytics-tdah.dw.d_pacientes` AS
+with pacientes AS (
 SELECT
   distinct FARM_FINGERPRINT(d.CNS_PAC) as id,
   d.CNS_PAC as cns,
+  d.DT_PROCESS as data_processamento,
   s.id as sexo_id,
   r.id as raca_id,
   m.id as municipio_id,
   e.id as etnia_id,
-  c.id as cid_id,
-  SAFE.PARSE_DATE('%y%m%d', d.DTNASC) AS data_nascimento,
+  d.CIDPRI as cid,
+  COALESCE(
+    SAFE.PARSE_DATE('%y%m%d', d.DTNASC),
+    SAFE.PARSE_DATE('%Y%m%d', d.DTNASC),
+    SAFE.PARSE_DATE('%y%m%e', d.DTNASC),
+    SAFE.PARSE_DATE('%Y%m%e', d.DTNASC)
+  ) AS data_nascimento,
   CASE
-    WHEN d.CIDPRI LIKE 'F90%' THEN TRUE
-    ELSE FALSE
+    WHEN d.CIDPRI LIKE '0000' THEN "CID Inválido"
+    WHEN d.CIDPRI LIKE 'F90%' THEN "SIM"
+    ELSE "NÃO"
   END AS tem_tdah,
+  ROW_NUMBER() OVER (PARTITION BY d.CNS_PAC ORDER BY d.DT_PROCESS DESC) AS rn
 FROM
   `self-service-analytics-tdah.datasus.d` as d
 LEFT JOIN `self-service-analytics-tdah.dw.d_sexos`       as s ON d.SEXOPAC = s.codigo
 LEFT JOIN `self-service-analytics-tdah.dw.d_racas`       as r ON d.RACACOR = r.codigo
 LEFT JOIN `self-service-analytics-tdah.dw.d_munincipios`  as m ON d.MUNPAC = CAST(m.codigo AS STRING)
 LEFT JOIN `self-service-analytics-tdah.dw.d_etnias`      as e ON d.ETNIA = e.codigo
-LEFT JOIN `self-service-analytics-tdah.dw.d_cids`        as c ON d.CIDPRI = c.codigo
+)
+select * except(rn) from pacientes WHERE rn = 1;
--- data_eng/bigquery/datasus/gold/pacientes.sql
+++ data_eng/bigquery/datasus/gold/pacientes.sql
@@ -1,16 +1,22 @@
 CREATE TABLE `self-service-analytics-tdah.gold.pacientes` AS
+with pacientes AS (
 SELECT
   p.id,
   p.cns,
   p.data_nascimento,
+  p.data_processamento,
+  p.cid,
   p.tem_tdah,
-  s.nome as sexo_id,
-  r.nome as raca_id,
-  m.nome as municipio_id,
-  e.nome as etnia_id,
+  s.codigo as sexo,
+  r.nome as raca,
+  m.nome as municipio,
+  e.nome as etnia,
+  ROW_NUMBER() OVER (PARTITION BY p.id ORDER BY p.data_processamento DESC) AS rn
 FROM
   `self-service-analytics-tdah.dw.d_pacientes` as p
 LEFT JOIN `self-service-analytics-tdah.dw.d_sexos`        as s ON p.sexo_id = s.id
 LEFT JOIN `self-service-analytics-tdah.dw.d_racas`        as r ON p.raca_id = s.id
 LEFT JOIN `self-service-analytics-tdah.dw.d_munincipios`  as m ON p.municipio_id = s.id
 LEFT JOIN `self-service-analytics-tdah.dw.d_etnias`       as e ON p.etnia_id = s.id
+)
+select * except(rn) from pacientes WHERE rn = 1;
